0dc1b2981c598e9f910cb79e95d6c0495fbf7ae4
Norbert Fabian <fnor1986@gmail.com>
2026-03-05 17:18:30 +0100

    Create tests/client-work.spec.ts

---
+++ tests/client-work.spec.ts
@@ -0,0 +1,6 @@
+// Initial structure for EPAM Client Work test
+import { test, expect } from '@playwright/test';
+
+test.describe('EPAM - Client Work navigation', () => {
+  // test implementation will be added in subsequent edits
+});
